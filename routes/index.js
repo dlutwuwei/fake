ce1b8015c9baf39c968670a333e4f839ed0bdbd8
wuwei <wuwei@bytedance.com>
2017-09-14 21:07:08 +0800

    disorder list

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const router = require('koa-router')()
 const request = require('koa2-request');
-
+const cheerio = require('cheerio');
 // router.get('/', async (ctx, next) => {
 //   await ctx.render('index', {
 //     title: 'Hello Koa 2!'
@@ -33,6 +33,36 @@ router.get('*', async (ctx) => {
   const response = await request(options);
   //Yay, HTTP requests with no callbacks!
   ctx.response.set(response.headers);
+  if(ctx.request.headers['accept'].indexOf('text/html') >= 0) {
+    const $ = cheerio.load(response.body);
+    if($('.gooditem')) {
+      const content = $('.gooditem');
+      randomArray(content);
+      const newContent = [];
+      content.each((i, item) => {
+        newContent[i] = $(item).html($(item));
+      });
+      $('.zkcontent').html(newContent.join(''))
+    }
+    $('head').append('<style>\
+    .darkbk { background-color: #B1191A;}\
+    </style>');
+    ctx.body = $.html();
+    return;
+  }
   ctx.body = response.body;
+
 })
 module.exports = router
+
+function randomArray(arr) {
+  for (i = 0; i < arr.length; i++) {
+    // 产生从 i 到 length 之间的随机数
+    index = parseInt(Math.random() * (arr.length - i)) + i;
+    if (index != i) {
+        temp = arr[i];
+        arr[i] = arr[index];
+        arr[index] = temp;
+    }
+  }
+}
